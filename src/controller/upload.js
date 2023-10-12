57a544fad6a9baf56fee39c15bc97a9081cfd07a
magic <1937134032@qq.com>
2023-10-12 18:02:00 +0800

    准备写数据库联动

--- src/controller/upload.js
+++ src/controller/upload.js
@@ -1,9 +1,10 @@
 const multer = require("koa-multer");
+const Files = require("../model/Files");
 
 let storage = multer.diskStorage({
   //文件保存路径 这个路由是以项目文件夹 也就是和入口文件（如app.js同一个层级的）
   destination: function (req, file, cb) {
-    cb(null, "files/photo");
+    cb(null, "files/photo/");
   },
   //修改文件名称
   filename: function (req, file, cb) {
@@ -14,9 +15,9 @@ let storage = multer.diskStorage({
 
 let upload = multer({
   storage: storage,
-//   limits: {
-//     fileSize: (1024 * 1024) / 2, // 限制512KB
-//   },
+  //   limits: {
+  //     fileSize: (1024 * 1024) / 2, // 限制512KB
+  //   },
 });
 
 module.exports = upload;
--- src/routes/upload.js
+++ src/routes/upload.js
@@ -4,10 +4,9 @@ const upload = require("../controller/upload");
 
 router.prefix("/upload");
 
-router.post("/photo", upload.single("file"), async (ctx, next) => {
+router.post("/photo", upload.single("file"), async (ctx, next) => {  // upload.single()中的名字要与前端formdata或postman中的key一致！
   try {
     // 在路由的时候注入，之后只要这个路由一走，就会自动的将我们的图片上传到服务器，并且保存到我们 upload 中间键配置的保存文件目录下
-    console.log("new data", ctx.req.file.filename);
     ctx.body = util.success(ctx.req.file.filename);
   } catch (error) {
     ctx.body = util.fail(error);
